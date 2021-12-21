f200d0032d22d39a1aa77c1db69762fd034fa866
Wadson Vaval <vavalson@hotmail.com>
2021-12-21 08:12:05 -0400

    🔥🗂 added firebase-cli and first test deployment.

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -5,7 +5,7 @@ function DashBoard() {
       <div>
         We’ve been waiting for you! WELCOME TO THE CONTRIBUTOR PANEL! Now that
         you’re here, let’s make the most of our time together. You’re one step
-        away from becoming a Freepik contributor and converting your resources
+        away from becoming a Collabo-stock contributor and converting your resources
         into earnings. This is what you have to do next: Upload and send your
         best resources. Quality is a must: let’s see how amazing you are. We
         believe in second chances, so you have two attempts to show your true
